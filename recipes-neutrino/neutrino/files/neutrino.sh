cfa05a5f60cdb91a998ce640ed96bc191a5e63cb
Markus Volk <flk@buildserver>
2017-11-23 18:10:37 +0100

    neutrino: align to new exit codes

--- recipes-neutrino/neutrino/files/neutrino.sh
+++ recipes-neutrino/neutrino/files/neutrino.sh
@@ -4,12 +4,12 @@ echo "### Starting Neutrino ###"
 /usr/bin/neutrino; RET=$?
 
 if [ $RET -eq 0 ]; then
-        echo "...Reboot" > /dev/dbox/oled0
-        systemctl reboot
+        :
 elif [ $RET -eq 1 ]; then
         echo "...Shutdown" > /dev/dbox/oled0
         systemctl poweroff
 elif [ $RET -eq 2 ]; then
-        :
+        echo "...Reboot" > /dev/dbox/oled0
+        systemctl reboot
 fi
 
