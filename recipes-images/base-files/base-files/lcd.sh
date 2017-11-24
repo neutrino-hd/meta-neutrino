4055e42ecf2974e1d11ef88b356731b6299347f7
Markus Volk <f_l_k@t-online.de>
2017-11-24 06:30:06 +0100

    Revert "lcd.sh: simplify"
    This reverts commit 22c3b1e686f36a5060c0459928e040fe5e93366c.

--- recipes-images/base-files/base-files/lcd.sh
+++ recipes-images/base-files/base-files/lcd.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-readlink -f /sys/class/block/sd[a-z]/device | grep ata && echo 1 > /proc/stb/lcd/symbol_hdd
-readlink -f /sys/class/block/sd[a-z]/device | grep usb && echo 1 > /proc/stb/lcd/symbol_usb
+if dmesg | grep -i SATA | grep -i "link up";then
+        echo 1 > /proc/stb/lcd/symbol_hdd
+fi
+
+if dmesg | grep -i USB | grep -i "USB Mass Storage device detected";then
+        echo 1 > /proc/stb/lcd/symbol_usb
+fi
