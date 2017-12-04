ef2c8ebc7ed830dee4d2a6f49d636fc444a7eeda
Markus Volk <flk@buildserver>
2017-12-04 15:34:07 +0100

    fix

--- recipes-images/base-files/base-files/createswap.sh
+++ recipes-images/base-files/base-files/createswap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 
-cat /proc/swaps >> /dev/null && echo "Swap already present" && exit 0
+cat /proc/swaps >> /dev/null && echo "Swap already present" && swapon /dev/mmcblk0p10 && exit 0
 
 mkswap /dev/mmcblk0p10
 swapon /dev/mmcblk0p10
