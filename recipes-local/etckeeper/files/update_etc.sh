fe51aed283bf82a62984e3fd0ece0fbcbb55b512
Markus Volk <f_l_k@t-online.de>
2018-05-20 23:20:59 +0200

    fix

--- recipes-local/etckeeper/files/update_etc.sh
+++ recipes-local/etckeeper/files/update_etc.sh
@@ -5,38 +5,38 @@ dev="HDD"
 mountpoint -q /media/USB && dev="USB"
 
 if [ $rootdevice = /dev/mmcblk0p3 ]; then
-        git commit GIT__URL="/media/$dev/service/partition1/git/etc.git"
+	GIT__URL="/media/$dev/service/partition1/git/etc.git"
 elif [ $rootdevice = /dev/mmcblk0p5 ]; then
-        GIT__URL="/media/$dev/service/partition2/git/etc.git"
+	GIT__URL="/media/$dev/service/partition2/git/etc.git"
 elif [ $rootdevice = /dev/mmcblk0p7 ]; then
-        GIT__URL="/media/$dev/service/partition3/git/etc.git"
+	GIT__URL="/media/$dev/service/partition3/git/etc.git"
 elif [ $rootdevice = /dev/mmcblk0p9 ]; then
-        GIT__URL="/media/$dev/service/partition4/git/etc.git"
+	GIT__URL="/media/$dev/service/partition4/git/etc.git"
 fi
 
 GIT_EXIST=$(echo $GIT__URL"/HEAD")
 
 if [ -e $GIT_EXIST ];then
-        if [ ! -e /etc/gitconfig ];then
-                git config --system user.name "GIT_USER"
-                git config --system user.email "MAIL"
-                git config --system core.editor "nano"
-                git config --system http.sslverify false
-        fi
-        if [ "$(cd $GIT__URL && git log -1 --pretty=format:"%cd")" == "$(cd /etc && git log -1 --pretty=format:"%cd")" ];then
-                exit
-        else
-                systemctl stop neutrino.service
-                echo "writing back /etc git remote from /dev/$dev"  > /dev/dbox/oled0
-                cd /etc && etckeeper init
-                git fetch -a
-                git reset --hard origin/master
-                echo "...done"
-                sync
-                etckeeper init
-                echo "rebooting"
-                systemctl reboot
-        fi
+	if [ ! -e /etc/gitconfig ];then
+		git config --system user.name "GIT_USER"
+		git config --system user.email "MAIL"
+		git config --system core.editor "nano"
+		git config --system http.sslverify false
+	fi
+	if [ "$(cd $GIT__URL && git log -1 --pretty=format:"%cd")" == "$(cd /etc && git log -1 --pretty=format:"%cd")" ];then
+		exit
+	else
+		systemctl stop neutrino.service
+		echo "writing back /etc git remote from /dev/$dev"  > /dev/dbox/oled0
+		cd /etc && etckeeper init
+		git fetch -a
+		git reset --hard origin/master
+		echo "...done"
+		sync
+		etckeeper init
+		echo "rebooting"
+		systemctl reboot
+	fi
 else
-        echo "no remote found"
+	echo "no remote found"
 fi
