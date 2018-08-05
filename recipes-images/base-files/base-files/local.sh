dea2805958ab4878effbdf54177f79e998193583
Markus Volk <f_l_k@t-online.de>
2018-08-05 12:03:48 +0200

    mount partitions rw by default

--- recipes-images/base-files/base-files/local.sh
+++ recipes-images/base-files/base-files/local.sh
@@ -8,11 +8,12 @@ if dmesg | grep -i USB | grep -i "USB Mass Storage device detected";then
         echo 1 > /proc/stb/lcd/symbol_usb
 fi
 
+## enable, if you want to mount the partitions read-only
 # link rootfs to /mnt
-for i in 1 2 3 4; do
-        if find /mnt/partition_$i -mindepth 1 | read; then
-               	:
-        else
-            	ln -sf /* /mnt/partition_$i
-        fi
-done
+#for i in 1 2 3 4; do
+#        if find /mnt/partition_$i -mindepth 1 | read; then
+#               	:
+#        else
+#            	ln -sf /* /mnt/partition_$i
+#        fi
+#done
