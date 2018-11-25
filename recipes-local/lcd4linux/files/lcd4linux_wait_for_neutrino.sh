d1e25664deb77f1663d93a32be6c323223e59e8a
Markus Volk <f_l_k@t-online.de>
2018-11-25 20:05:48 +0100

    simplify

--- recipes-local/lcd4linux/files/lcd4linux_wait_for_neutrino.sh
+++ recipes-local/lcd4linux/files/lcd4linux_wait_for_neutrino.sh
@@ -6,7 +6,7 @@ file="$monitordir/layout"
 [ -d "$monitordir" ] || mkdir -p "$monitordir"
 
 while [ ! -f "$file" ]; do
-	inotifywait -e create -e modify "$(dirname $file)"
+	inotifywait -e create -e modify "$monitordir"
 done
 
 exit 0
